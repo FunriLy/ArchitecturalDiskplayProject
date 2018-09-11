46901cd316d2fba8f74fd7c6609bfb4391f56087
FunriLy <fr479910770@qq.com>
2018-09-11 16:03:04 +0800

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+### Architectural Diskplay Project
+
+> 一个个人训练项目后端代码仓库！！！
+> 未经允许，不得使用！！！
---
+++ src/main/java/com/qg/fangrui/ArchitecturalDiskplayProject/ArchitecturalDiskplayProjectApplication.java
@@ -0,0 +1,12 @@
+package com.qg.fangrui.ArchitecturalDiskplayProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturalDiskplayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitecturalDiskplayProjectApplication.class, args);
+	}
+}
